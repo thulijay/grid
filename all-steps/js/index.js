8cac4d8858cdb3b619eab2268ef18e9631c8100a
thulijay <thulijay79@gmail.com>
2020-09-29 15:58:03 +0200

    fixed

---
+++ all-steps/js/index.js
@@ -0,0 +1,15 @@
+
+    JSC.Chart('chartDiv', {
+        type: 'line',
+        series: [
+           {
+              points: [
+                 {x: 'Monday', y: 3},
+                 {x: 'Tuesday', y: 4},
+                 {x: 'Wednesday', y: 5},
+                 {x: 'Thursday', y: 2},
+                 {x: 'Friday', y: 5}
+              ]
+           }
+        ]
+     });
--- all-steps/step1-b/js/index.js
+++
@@ -1,1 +0,0 @@
-JSC.Chart('chartDiv', {});
